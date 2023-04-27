9b6380a4d7366e1705f094c064813d294e054579
LSilva <lucas@shifta.la>
2023-04-27 03:59:24 -0300

    Añadiendo apellidos

--- PlanCarrera/PlanCarrera.DataAccess/DataContext.cs
+++ PlanCarrera/PlanCarrera.DataAccess/DataContext.cs
@@ -31,11 +31,11 @@ namespace PlanCarrera.DataAccess
         {
             var personas = new List<Persona>()
             {
-                new Persona(){ Id = 1, Nombre = "Jhon", Edad = 22, DNI = "42901989", Sexo = "Masculino" },
-                new Persona(){ Id = 2, Nombre = "Tomy", Edad = 20, DNI = "42901459", Sexo = "Masculino" },
-                new Persona(){ Id = 3, Nombre = "Jade", Edad = 24, DNI = "42456989", Sexo = "Femenino" },
-                new Persona(){ Id = 4, Nombre = "Emily", Edad = 27, DNI = "42452989", Sexo = "Femenino" },
-                new Persona(){ Id = 5, Nombre = "Alex", Edad = 18, DNI = "42789689", Sexo = "Femenino" },
+                new Persona(){ Id = 1, Nombre = "Jhon Keneddy", Edad = 22, DNI = "42901989", Sexo = "Masculino" },
+                new Persona(){ Id = 2, Nombre = "Tomy Shelby", Edad = 20, DNI = "42901459", Sexo = "Masculino" },
+                new Persona(){ Id = 3, Nombre = "Jade Classius", Edad = 24, DNI = "42456989", Sexo = "Femenino" },
+                new Persona(){ Id = 4, Nombre = "Emily Blunt", Edad = 27, DNI = "42452989", Sexo = "Femenino" },
+                new Persona(){ Id = 5, Nombre = "Alex Mount", Edad = 18, DNI = "42789689", Sexo = "Femenino" },
             };
 
             // Convertir el array de objetos JSON a una cadena JSON
